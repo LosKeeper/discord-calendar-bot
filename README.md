d5a46ebc3c58871d82ccd1d8f473d4881bead571
unknown <thomas.dumond2001@gmail.com>
2022-04-26 23:57:53 +0200

    README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Discord Calendar Bot
+> The bot is currently in development.
+
+> Many libraries are needed to make this bot work.
+```pwsh
+pip install -r requirements.txt
+```
+
+
+## Configuration
+To configure the bot, you need to create a configuration file call `config.py`.
+```python
+url = # URL of the calendar in ics format 
+token = # Token of the bot 
+```
+
+## Functions
+The bot use the slash commands to interact with the user.
+| Command             | Description                 |
+| :------------------ | :-------------------------- |
+| `/help`             | Show the help message       |
+| `/today`            | Show the events of today    |
+| `/tomorrow`         | Show the events of tomorrow |
+| `/week`             | Show the events of the week |
+| `/day <YYYY-MM-DD>` | Show the events of a day    |
+
+## TODO
+- Create help command
+- Create day command with slash interactions
--- calendar-bot.py
+++ calendar-bot.py
@@ -15,29 +15,29 @@ async def on_ready():
     print('Logged in as {0.user}'.format(bot))
 
 
-@slash.slash(name='jour', description='Affiche les événements du jour')
-async def _jour(ctx: SlashContext):
+@slash.slash(name='today', description='Affiche les événements du jour')
+async def _today(ctx: SlashContext):
     embed = discord.Embed(title="Cours du jour",
                           description=day_events(0), color=0x00ff00)
     await ctx.send(embed=embed)
 
 
-@slash.slash(name='demain', description='Affiche les événements de demain')
-async def _demain(ctx: SlashContext):
+@slash.slash(name='tomorrow', description='Affiche les événements de demain')
+async def _tomorrow(ctx: SlashContext):
     embed = discord.Embed(title="Cours de demain",
                           description=day_events(1), color=0x00ff00)
     await ctx.send(embed=embed)
 
 
-@slash.slash(name='semaine', description='Affiche les événements de la semaine')
-async def _semaine(ctx: SlashContext):
+@slash.slash(name='week', description='Affiche les événements de la semaine')
+async def _week(ctx: SlashContext):
     embed = discord.Embed(title="Cours de la semaine", description=day_events(
         0)+day_events(1)+day_events(2)+day_events(3)+day_events(4)+day_events(5)+day_events(6)+day_events(7), color=0x00ff00)
     await ctx.send(embed=embed)
 
 
-@slash.slash(name='date', description='Affiche les événements de la date')
-async def _date(ctx: SlashContext, user_input: str = ""):
+@slash.slash(name='day', description='Affiche les événements de la date')
+async def _day(ctx: SlashContext, user_input: str = ""):
     try:
         str = datetime.strptime(str, '%Y-%m-%d')
     except ValueError:
